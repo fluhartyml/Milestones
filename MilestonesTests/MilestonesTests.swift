7ee84b1258989dd630ff61ce6eb55c4fb6528f3e
Michael Fluharty <michael@fluharty.com>
2026-04-01 18:54:26 -0500

    Initial Commit

---
+++ MilestonesTests/MilestonesTests.swift
@@ -0,0 +1,16 @@
+//
+//  MilestonesTests.swift
+//  MilestonesTests
+//
+//  Created by Michael Fluharty on 4/1/26.
+//
+
+import Testing
+
+struct MilestonesTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
